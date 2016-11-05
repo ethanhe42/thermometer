d6a3a46dcf8d7d0ebf027c40c32a319f8264b379
yihui-he <yihuihe@foxmail.com>
2016-11-05 21:27:24 +0800

    temp

---
+++ Debug/Temperature_Sense_Demo.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+Temperature_Sense_Demo.obj: ../Temperature_Sense_Demo.c
+Temperature_Sense_Demo.obj: D:/Program\ Files\ (x86)/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+Temperature_Sense_Demo.obj: D:/Program\ Files\ (x86)/ti/ccsv5/ccs_base/msp430/include/in430.h
+Temperature_Sense_Demo.obj: D:/Program\ Files\ (x86)/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+
+../Temperature_Sense_Demo.c: 
+D:/Program\ Files\ (x86)/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+D:/Program\ Files\ (x86)/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+D:/Program\ Files\ (x86)/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2553.cmd 
+
+C_SRCS += \
+../Temperature_Sense_Demo.c 
+
+OBJS += \
+./Temperature_Sense_Demo.obj 
+
+C_DEPS += \
+./Temperature_Sense_Demo.pp 
+
+C_DEPS__QUOTED += \
+"Temperature_Sense_Demo.pp" 
+
+OBJS__QUOTED += \
+"Temperature_Sense_Demo.obj" 
+
+C_SRCS__QUOTED += \
+"../Temperature_Sense_Demo.c" 
+
+
